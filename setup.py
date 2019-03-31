e48046145dbe2e4856d192d07c84a7bdf8bf9956
Reid Sanders <reidsanders@gmail.com>
2019-03-31 14:01:58 -0400

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = 'danbooru-utility',
-    version = '0.1.19',
+    version = '0.1.20',
     url = 'https://github.com/reidsanders/danbooru-utility.git',
     author = 'Reid Sanders',
     author_email = 'reid@reidsanders.net',
